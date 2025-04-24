da84ca1e6ebab3efb11b74b9001f525558c95bc4
Jhonatan Oliveira <jhonatanvelaskii@icloud.com>
2025-04-24 16:57:07 -0300

    Configura GitHub Pages com Vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/fashion-ecoomerce/'
+  base: '/fashion-ecoomerce/',
+  plugins: [react()]
 })
